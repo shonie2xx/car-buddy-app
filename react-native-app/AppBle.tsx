190c4694377930742e58af9dc3f08d1d8240978d
shonie2xx <aleksandarlekov11@gmail.com>
2023-01-12 21:02:52 +0100

    I MADE IT

--- react-native-app/AppBle.tsx
+++ react-native-app/AppBle.tsx
@@ -15,63 +15,13 @@ import { BleManager, Device } from "react-native-ble-plx";
 const bleManager = new BleManager();
 
 const App = () => {
-  const [allDevices, setAllDevices] = useState<Device[]>([]);
-  //const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
-
-  //   useEffect(() => {
-  //     scanForDevice();
-  //   },
-  //   [])
-
-  // //check for duplicates
-  // const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
-  //   devices.findIndex(device => nextDevice.id === device.id) > -1;
-
-  // const scanForDevice = () => {
-  //   bleManager.startDeviceScan(null, null, (error, device) => {
-  //     if (error) {
-  //       console.log(error);
-  //     }
-  //     if (device && device.name?.includes('B2-GreenDMS')) {
-  //       setAllDevices((prevState: Device[]) => {
-  //         if (!isDuplicteDevice(prevState, device)) {
-  //           return [...prevState, device];
-  //         }
-  //         return prevState;
-  //       });
-  //     }
-  // })
-  // };
-
-  // const connectToDevice = async (device: Device) => {
-  //   try {
-  //     console.log("DEVICE: ", device.name + device.id)
-  //     await device.cancelConnection();
-  //     const deviceConnection = await bleManager.connectToDevice(device.id);
-  //     setConnectedDevice(deviceConnection);
-  //     await deviceConnection.discoverAllServicesAndCharacteristics();
-  //     bleManager.stopDeviceScan();
-  //     console.log("connected device", deviceConnection)
-  //     //startStreamingData(deviceConnection);
-  //   } catch (e) {
-  //     console.log('FAILED TO CONNECT', e);
-  //   }
-  // };
-
-  // const disconnectFromDevice = () => {
-  //   if (connectedDevice) {
-  //     bleManager.cancelDeviceConnection(connectedDevice.id);
-  //     setConnectedDevice(null);
-  //   }
-  // };
-
-  const [scanedDevice, setScannedDevice] = useState<Device | null>(null);
+  const [scannedDevice, setScannedDevice] = useState<Device | null>(null);
   const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
-  
+  const [uuid, setUUID] = useState("");
   const scanForDevices = async () => {
     bleManager.startDeviceScan(null, null, (error, device) => {
       console.log("Scanning...")
-      console.log(device);
+      //console.log(device);
 
       if (error) {
         console.log("Error while scanning :", error)
@@ -81,75 +31,55 @@ const App = () => {
       if (device && device.name?.includes('B2-GreenDMS')) {
         console.log("Connecting to hm-10");
         bleManager.stopDeviceScan();
+        device.serviceUUIDs?.map((item) => setUUID(item));
         setScannedDevice(device);
       }
     })
   }
 
   const connectToDevice = async () => {
-    if(scanedDevice) {
-      const connected = await scanedDevice.connect();
+    if(scannedDevice) {
+      console.log("id", scannedDevice.id);
       
-      console.log("Discovering services and characteristics");
-      await connected.discoverAllServicesAndCharacteristics();
-      setConnectedDevice(connected);
-      const services = await connected.services();
-      services.forEach(async service => {
-        const characteristics = await connected.characteristicsForService(service.uuid);
-        characteristics.forEach(console.log);
-      });
-      //console.log("INFO", info);
-//       await device.discoverAllServicesAndCharacteristics();
-// const services = await device.services();
-// services.forEach(async service => {
-//    const characteristics = await device.characteristicsForService(service.uuid);
-//    characteristics.forEach(console.log);
-// });
+      const device = await bleManager.connectToDevice(scannedDevice.id, {requestMTU: 187});
+      await device.discoverAllServicesAndCharacteristics();
+      
+      console.log("services", await device.services())
+
     }
   }
 
-  const sendCommand = async () => {
-    connectedDevice?.writeCharacteristicWithResponseForService('0000ffe0-0000-1000-8000-00805f9b34fb','0xFFE0',"MQ==")
+  const sendCommand1 = async () => {
+    //console.log("connected device and sending", scannedDevice)
+    // scannedDevice?.wri('0000ffe0-0000-1000-8000-00805f9b34fb','0xFFE1','Ng==')
+    console.log("E tva kvo e? : ", await scannedDevice?.characteristicsForService("0000ffe0-0000-1000-8000-00805f9b34fb"))
+    scannedDevice?.writeCharacteristicWithResponseForService(
+      "0000ffe0-0000-1000-8000-00805f9b34fb",
+      "0000ffe1-0000-1000-8000-00805f9b34fb",
+      "MQ=="
+    )
   }
-//10795694912
-//0000ffe0-0000-1000-8000-00805f9b34fb
-  const getInfo = async () => {
-   
+  const sendCommand2 = async () => {
+    //console.log("connected device and sending", scannedDevice)
+    // scannedDevice?.wri('0000ffe0-0000-1000-8000-00805f9b34fb','0xFFE1','Ng==')
+    console.log("E tva kvo e? : ", await scannedDevice?.characteristicsForService("0000ffe0-0000-1000-8000-00805f9b34fb"))
+    scannedDevice?.writeCharacteristicWithResponseForService(
+      "0000ffe0-0000-1000-8000-00805f9b34fb",
+      "0000ffe1-0000-1000-8000-00805f9b34fb",
+      "Mg=="
+    )
   }
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         
         <Button title="Scan" onPress={scanForDevices} />
 
-        <Text >Devices available: {scanedDevice?.name}</Text>
+        <Text >Devices available: {scannedDevice?.name}</Text>
         <Button title="Connect" onPress={connectToDevice} />
-        <Button title="Send" onPress={sendCommand} />
-        {/* <View>
-            { allDevices.map((device, index) => 
-              {
-                return (
-                 
-                  <View key={index}>
-                  <Text>{device.name}</Text>
-                  { connectedDevice ? 
-                  <Button title="Disconnect" onPress={disconnectFromDevice}/>  
-                  :
-                  <Button title="Connect" onPress={() => connectToDevice(device)}/>
-                }
-                </View>
-                )
-              }
-              )}
-          </View> */}
-        {/* {device ? (
-          <Button title="Connect" onPress={connectToDevice} />
-        ) : (
-          <Button
-            title="Can't connect"
-            onPress={() => console.log("can't connect")}
-          />
-        )} */}
+        <Button title="Light On" onPress={sendCommand1} />
+        <Button title="Light Of" onPress={sendCommand2} />
       </View>
     </SafeAreaView>
   );
